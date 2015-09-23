a2c28b346f568566e40d6375262542f7e3978b46
Zhiren Guan <jasong327@hotmail.com>
2015-09-23 16:37:45 +1000

    Added Basic Unity Architecture

---
+++ Managers/LocalDataManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LocalDataManager : AutoSingletonManager<LocalDataManager> {
+
+	public static string TYPE_BOOLEAN = "Boolean";
+	public static string TYPE_INT = "Int";
+	public static string TYPE_FLOAT = "Float";
+	public static string TYPE_STRING = "String";
+
+
+
+	private void setData(string key, object value, string type){
+
+		if (type == TYPE_BOOLEAN) {
+			bool convertedValue = Convert.ToBoolean(value);
+			PlayerPrefs.SetInt (key, convertedValue == true ? 1 : 0);
+		} else if (type == TYPE_INT) {
+			int convertedValue = Convert.ToInt32(value);
+			PlayerPrefs.SetInt (key, convertedValue);
+		} else if (type == TYPE_FLOAT) {
+			float convertedValue = (float) value;
+			PlayerPrefs.SetFloat (key, convertedValue);
+		} else {
+			string convertedValue = Convert.ToString(value);
+			PlayerPrefs.SetString (key, convertedValue);
+		}
+	}
+
+	private object getData(string key, string type){
+
+		if (type == TYPE_BOOLEAN) {
+			return PlayerPrefs.GetInt (key) == 1 ? true : false;
+		} else if (type == TYPE_INT) {
+			return PlayerPrefs.GetInt (key);
+		} else if (type == TYPE_FLOAT) {
+			return PlayerPrefs.GetFloat (key);
+		} else {
+			return PlayerPrefs.GetString (key);
+		}
+	}
+}
---
+++ Utils/GameConsts.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameConsts {
+	
+	public static string APPLE_ID = "1001";
+	public static string ANDROID_URL = "com.temptzon.relicsofslayers";
+}
